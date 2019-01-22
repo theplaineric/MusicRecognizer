e9b2885f910307e3596022338fcf61bcb00137eb
Eric <theplaineric@gmail.com>
2019-01-22 10:35:36 -0800

    Processed WAV files

--- Music Preprocessing/DataPreprocessing.py
+++ Music Preprocessing/DataPreprocessing.py
@@ -1,11 +1,11 @@
 # coding: utf-8
 import pandas as pd
-from pydub import Audio Segment
+from pydub import AudioSegment
 from glob import iglob
 import os
 # os.path.isfile('')
 
-data = pd.read_csv("LibraryList.csv")
+data = pd.read_csv("C:/Users/theplaineric/Desktop/MusicRecognizer/LibraryList.csv")
 
 def locationProcessing(str):
     sample = "file://localhost/"
@@ -22,3 +22,30 @@ def isWav(str):
 data['ext'] = data['path'].apply(isWav)
 data['ext'].value_counts()
 df = data[(data.ext == 'mp3') | (data.ext == 'wav')]
+
+mp3_files = df[df['ext'] == 'mp3']['path'].tolist()
+
+def convert_mp3_to_wav():
+    for file in mp3_files:
+        if (not os.path.isfile(file)):
+            continue
+        mp3_to_wav = AudioSegment.from_mp3(file)
+        mp3_to_wav.export(get_file_name(file), format='wav')
+
+def get_file_name(str):
+    strList = str.split("/")
+    folder_destination = "C:/Users/theplaineric/Desktop/MusicRecognizer/Music Samples/"
+    return folder_destination+strList[-1][:-4]+'.wav'
+
+convert_mp3_to_wav()
+
+"""
+def delete_wav_files():
+    for file in mp3_files:
+        if (not os.path.isfile(file)):
+            continue
+        delete_wav_file = file[:-4]+'wav'
+        os.remove(delete_wav_file)
+
+delete_wav_files()
+"""
